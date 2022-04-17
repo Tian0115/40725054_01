0f1fadd9d69a84ba67b250e0130db74b93239f95
Tian0115 <100185858+Tian0115@users.noreply.github.com>
2022-04-17 19:30:05 +0800

    attack

---
+++ 40725054_01/Assets/(Script)/DataWeapon.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Tian
+{
+    /* Data
+     武器資料
+    1.飛行速度
+    2.攻擊力
+    3.起始數量
+    4.最大數量
+    5.間隔時間
+    6.生成位置
+    //ScriptableObject 腳本化物件
+    //作用：將腳本的資料變成物件儲存於 Project 內 (擴充和維護性佳)
+    //CreateAssetMenu 與 ScriptableObject 搭配使用
+    //作用：在Project 建立此腳本化物件的選單與檔案名稱
+    //menuName 選單名稱、fileName 檔案名稱
+     */
+    [CreateAssetMenu(menuName = "Tian/Data Weapon", fileName = "Data Weapon")]
+    public class DataWeapon : ScriptableObject
+    {
+        [Header("飛行速度"), Range(0, 5000)]
+        public float speed = 500;
+        [Header("攻擊力"), Range(0, 100)]
+        public float attack = 10;
+        [Header("起始數量"), Range(1, 30)]
+        public int countStart = 15;
+        [Header("最大數量"), Range(1, 100)]
+        public int countMax = 25;
+        [Header("間隔時間"), Range(0, 3)]
+        public float interval = 1.2f;
+        [Header("生成位置")]
+        public Vector3[] v3SpawnPoint;
+    }
+
+}
+
---
+++ 40725054_01/Assets/(Script)/TopDownController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+
+//命名空間 空間名稱
+// 多人開發可以使用命名空間區隔系統避免衝突
+   namespace Tian
+  {
+    public class TopDownController : MonoBehaviour
+    {
+        #region 資料：保存系統需要的基本資料：移動速度、動畫參數名稱與元件等
+        //
+        [SerializeField, Header("移動速度"), Range(0, 100)]
+        private float speed = 10.5f;
+        private string parameterRun = "walk";
+        private string parameterDead = "dead";
+        private Animator ani;
+        private Rigidbody2D rig;
+        private float h;
+        private float v;
+
+        #endregion
+
+        #region 事件：程式入口 (Unity)，提供開發者驅動系統的窗口
+
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+            rig = GetComponent<Rigidbody2D>();
+        }
+        private void Update()
+        {
+            GetInput();
+            Move();
+        }
+
+        #endregion
+
+        #region 方式：較複雜的功能 
+        private void GetInput()
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+        }
+        private void Move()
+        {
+            rig.velocity = new Vector2(h, v) * speed;
+
+            ani.SetBool(parameterRun, h != 0 || v != 0);
+
+            #region 三元運算
+            //三元運算
+            /*官方文件 : https://docs.microsoft.com/zh-tw/dotnet/csharp/language-reference/operators/conditiional-operator  */
+            //布林值 ? 布林值 = true : 布林值 = false
+            //水平 >=0 角度 0 否則 角度 180
+            //變形元件.尤拉角(eulerAngles) = 新 三維向量(x, y, z)
+            #endregion
+
+            transform.eulerAngles = new Vector3(0, h >= 0 ? 0 : 180, 0);
+        }
+        #endregion
+    }
+}
+
+
